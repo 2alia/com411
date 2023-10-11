9dd90563feb37aae87d6398b515665449e2808c5
2alia <146340747+2alia@users.noreply.github.com>
2023-10-11 21:56:14 +0100

    Create week 3

---
+++ simple_loop.py
@@ -0,0 +1,20 @@
+'''for count in range(0, 1, 1):
+    print("7")
+    print("6")
+    print("5")
+    print("4")
+    print("3")
+    print("2")
+    print("1")
+    '''
+'''print("what level of brightness is required?")
+range(0,10,2)
+    print("**")
+    print("****")
+    print("******")
+'''
+print("Please enter a word")
+user_word = input()
+
+for position in range(0, len(user_word), 1):
+    print(user_word[position])
