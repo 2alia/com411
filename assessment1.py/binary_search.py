5ff1ddbd5fe579c7a56eaa9c801fd79e0ab0d24f
2alia <146340747+2alia@users.noreply.github.com>
2023-12-13 01:28:02 +0000

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	assessment1.py/Graph.py

--- assessment1.py/binary_search.py
+++ assessment1.py/binary_search.py
@@ -1,40 +1,16 @@
-
-def hotel_search(list, search_term_hotel,search_term_breakfast,search_term_bed, hotel_type):
+def hotel_search(list,search_term_hotel,search_term_breakfast,search_term_bed, hotel_type):
     start = 0
     end = len(list)-1
 
     found = False
 
-    while found == False and end >= start:
+    while found <= False and end >= start:
         midpoint = (start + end) // 2
 
-        if list[midpoint] == search_term_hotel:
-            found = True
-        elif search_term_hotel < list[midpoint]:
-            end = midpoint - 1
-        else: start = midpoint + 1
-
-        if list[midpoint] == search_term_breakfast:
-            found = True
-        elif search_term_breakfast < list[midpoint]:
-            end = midpoint - 1
-        else:
-            start = midpoint + 1
-
-        if list[midpoint] == search_term_bed:
-            found = True
-        elif search_term_bed < list[midpoint]:
-            end = midpoint - 1
-        else:
-            start = midpoint + 1
-
-        if list[midpoint] == hotel_type:
+        if list[midpoint] == hotel_search(search_term_hotel,search_term_breakfast,search_term_bed, hotel_type):
             found = True
         elif hotel_type < list[midpoint]:
             end = midpoint - 1
         else:
             start = midpoint + 1
-
-
-
     return found
--- assessment1.py/main2.py
+++ assessment1.py/main2.py
@@ -1,6 +1,5 @@
 from binary_search import hotel_search
 
-
 place_type = ["liverpool", "manchester", "Sutton"]
 Hotel_list = ["Sutton B&B", "Liverpool B&B", "Manchester B&B"]
 breakfast_list = ["pancakes", "omelette", "french_toast","overnight_oats"]
@@ -10,7 +9,7 @@ search_term_hotel = input("enter hotel name: e.g. Sutton B&B, Liverpool B&B, Man
 search_term_breakfast = input("enter breakfast:e.g. pancakes, omelette, french_toast , overnight_oats")
 search_term_bed = int(input("enter number of beds needed:e.g. 1, 2 , 3 bedrooms"))
 
-found = hotel_search(search_term_hotel,search_term_breakfast,search_term_bed, hotel_type)
+found = hotel_search(hotel_type,search_term_hotel,search_term_breakfast,search_term_bed)
 
 if not found:
     print(f"{search_term_hotel}, {search_term_breakfast}, {search_term_bed}, {hotel_type} unknown entry")
