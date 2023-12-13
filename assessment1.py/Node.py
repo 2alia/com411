0bbd770bc286807004cf326bd0a768c3f795b486
2alia <146340747+2alia@users.noreply.github.com>
2023-12-13 00:33:43 +0000

    Delete assessment1.py/Node.py

--- assessment1.py/Node.py
+++
@@ -1,7 +0,0 @@
-class node:
-    def __init__(self, value):
-        self.value =value
-        self.adjacent_nodes = []
-
-    def add_neighbour(self, neighbour_node):
-        self.adjacent_nodes.append(neighbour_node)
